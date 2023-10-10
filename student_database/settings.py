425e1ad5dbe0c9d3a6dab3108f3f9dee04afed00
ankur1670 <16sainiankur@gmail.com>
2023-10-10 18:43:28 +0530

    Feedback button

--- student_database/settings.py
+++ student_database/settings.py
@@ -162,6 +162,21 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+
+        'rest_framework.throttling.AnonRateThrottle',
+
+        'rest_framework.throttling.UserRateThrottle'
+
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+
+        'anon': '5/min',
+
+        'user': '20/min'
+
+    }
 
 }
