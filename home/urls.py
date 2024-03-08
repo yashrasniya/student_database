9331fdb781dc2dcaa4c6e9deb7845f05aff8cdd1
Sarthak Joshi <100042684+Craniace@users.noreply.github.com>
2024-03-09 01:59:46 +0530

    Home is completed from menu,news,events,contact,tophead.

--- home/urls.py
+++ home/urls.py
@@ -1,21 +1,24 @@
-#urls.py
+# urls.py
 from django.urls import path, include
 from rest_framework import routers
-from .views import MissionAPIView, VisionAPIView, ActivityImageAPIView, AnnouncementViewSet, CourseViewSet, FooterLinkViewSet
+from .views import MissionAPIView, VisionAPIView, ActivityImageAPIView, AnnouncementViewSet, CourseViewSet, \
+    FooterLinkViewSet, MenuAPIView, NewsAPIView, EventAPIView, ContactAPIView, TopHeaderLinkList
 
+app_name = 'home'
 
 router = routers.DefaultRouter()
-router.register(r'announcements', AnnouncementViewSet)
-router.register(r'courses', CourseViewSet)
-router.register(r'footer-links', FooterLinkViewSet)
+router.register(r'announcements', AnnouncementViewSet, basename='announcement')
+router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'footer-links', FooterLinkViewSet, basename='footer-link')
 
 urlpatterns = [
     path('mission/', MissionAPIView.as_view(), name='mission-api'),
     path('vision/', VisionAPIView.as_view(), name='vision-api'),
     path('activity-images/', ActivityImageAPIView.as_view(), name='activity-images-api'),
     path('api/', include(router.urls)),
-    path('api/', include(router.urls)),
-    path('api/', include(router.urls)),
-    # Add other URLs as needed
+    path('menu/', MenuAPIView.as_view(), name='menu-api'),
+    path('news/', NewsAPIView.as_view(), name='news-api'),
+    path('events/', EventAPIView.as_view(), name='events-api'),
+    path('contact/', ContactAPIView.as_view(), name='contact-api'),
+     path('topheader/', TopHeaderLinkList.as_view(), name='topheader-list'),
 ]
-
