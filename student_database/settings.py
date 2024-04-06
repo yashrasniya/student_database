53197f716fe6c47478404f833bb9280734b5e92c
Sarthak Joshi <100042684+Craniace@users.noreply.github.com>
2024-04-07 00:23:25 +0530

    post pass added in home

--- student_database/settings.py
+++ student_database/settings.py
@@ -98,10 +98,11 @@ WSGI_APPLICATION = 'student_database.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
