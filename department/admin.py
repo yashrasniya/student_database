75d990bd94d67d76838950cb427ee691bfe2af44
Virendra Singh Rana <75882773+bop578530@users.noreply.github.com>
2024-04-28 13:40:45 +0530

    Changes

--- department/admin.py
+++ department/admin.py
@@ -13,7 +13,7 @@ class ImageInline(admin.TabularInline):
 
 class WorkingCommunitiesInline(admin.TabularInline):
     model = Working_communities
-    extra= 0
+    extra= 1
 
 class DepartmentAdmin(admin.ModelAdmin):
     inlines = [
--- event/models.py
+++ event/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here
 
 class Event(models.Model):
-    event_img= models.ImageField(upload_to=Event, height_field=None, width_field=None, max_length=None)
+    event_img= models.ImageField(upload_to='EventImage', height_field=None, width_field=None, max_length=None)
