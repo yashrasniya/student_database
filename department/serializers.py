a334c01cb54cf15d2e7f135bd679a2cfabe2546e
Yash <yashrasniya3@gmail.com>
2024-05-01 10:17:10 +0530

    add lab section

--- department/serializers.py
+++ department/serializers.py
@@ -1,27 +1,44 @@
 from rest_framework import serializers
 from .models import *
 
+
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = '__all__'
 
-class LabsSerializer(serializers.ModelSerializer):
+class PracticeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Labs
+        model = Practical
         fields = '__all__'
 
+
+class LabSerializer(serializers.ModelSerializer):
+    practical = PracticeSerializer(many=True)
+
+    class Meta:
+        model = Lab
+        fields = '__all__'
+
+
+
 class DepartmentSerializer(serializers.ModelSerializer):
+    department_labs_name=LabSerializer(many=True)
     class Meta:
         model = Department
         fields = '__all__'
+
 
 class NavBarSerializer(serializers.ModelSerializer):
     class Meta:
         model = NavBar
         fields = '__all__'
 
+
 class WorkingCommunitiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Working_communities
         fields = '__all__'
+
+
+
